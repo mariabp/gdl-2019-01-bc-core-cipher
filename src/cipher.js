c99eda089994e68ff2fb6d9059dc9f6a9682d74e
Maria Bolanos <Maria@99.200-57-1.bestel.com.mx>
2019-01-29 11:53:15 -0600

    Agregue html y funcion encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,22 @@
 window.cipher = {
   // ... 
 };
+
+const encode = (message, offset) => {
+  let output = "";
+/*Como se que esta poniendo cada caracter?,*/
+  for (let i = 0; i < message.length; i++) {
+    let result = message[i];
+    let code = message.charCodeAt(i);
+    if (code >= 65 && code <= 90)
+      result = String.fromCharCode((code - 65 + offset) % 26 + 65);
+
+    output += result;
+  }
+
+  return output;
+};
+
+
+
+
