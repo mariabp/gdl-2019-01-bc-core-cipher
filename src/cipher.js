ca975eb9b75db183d0960be94accd442b221e45a
Maria Bolanos <Maria@99.200-57-1.bestel.com.mx>
2019-02-01 17:12:44 -0600

    Hoy encode y decode ya pasaron los test

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,72 @@
-let output="";
 
-
+let cipheredLetter = '';
+let cipheredPhrase = '';
+let decipheredLetter = '';
+let decipheredPhrase = '';
 
 window.cipher = {
-    encode : (string, offset) => {
+    encode : (offset, string) => {
+      for (let i = 0; i < string.length; i++) {
+        let currentLetter = string.charAt(i);
     
-      for (let i = 0; i < string.length; i++) {
-        let result = string[i];
-        let code = string.charCodeAt(i);
-        result = String.fromCharCode((code - 65 + offset) % 26 + 65);
-        output += result;
-        cipheredText.innerHTML = output;
-        
+        let letterCode = currentLetter.charCodeAt(0);
+    
+        if (letterCode >= 65 && letterCode <= 90) {
+          cipheredLetter = String.fromCharCode(
+            (letterCode + offset - 65) % 26 + 65
+          );
+    
+          cipheredPhrase += cipheredLetter;
+        } else if (letterCode >= 97 && letterCode <= 122) {
+          cipheredLetter = String.fromCharCode(
+            (letterCode + offset - 97) % 26 + 97
+          );
+    
+          cipheredPhrase += cipheredLetter;
+        } 
+          else {
+          cipheredLetter = String.fromCharCode(letterCode);
+    
+          cipheredPhrase += cipheredLetter;
+        }
       }
+    
+      return cipheredPhrase;
+    },
      
-      return output;
-     },
-     
-    decode : (string, offset) => {
-      
+    decode : (offset, string) => {
       for (let i = 0; i < string.length; i++) {
-        let result = string[i];
-        let code = string.charCodeAt(i);
-          result = String.fromCharCode((code + 65 - offset) % 26 + 65);
-     
-        output += result;
-        cipheredText.innerHTML = output;
+        let currentLetter = string.charAt(i);
+    
+        let letterCode = currentLetter.charCodeAt(0);
+    
+        if (letterCode >= 65 && letterCode <= 90) {
+          decipheredLetter = String.fromCharCode(
+            (letterCode - offset - 90) % 26 + 90
+          );
+    
+          decipheredPhrase += decipheredLetter;
+        } else if (letterCode >= 97 && letterCode <= 122) {
+          decipheredLetter = String.fromCharCode(
+            (letterCode - offset - 122) % 26 + 122
+          );
+    
+          decipheredPhrase += decipheredLetter;
+        } else {
+          decipheredLetter = String.fromCharCode(letterCode);
+    
+          decipheredPhrase += decipheredLetter;
+        }
       }
-     
-      return output;
-     }
+    
+      return decipheredPhrase;
+    }
+  
   
   
 };
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-let string = document.querySelector('#text');
-let offset = document.querySelector('#choice');
+// let string = document.querySelector('#text');
+// let offset = parseInt(document.querySelector('#choice'));
 let cipherSubmit = document.querySelector('#ciphersubmit');
-let cipheredText = document.querySelector('#cipheredtext');
+// let cipheredText = document.querySelector('#cipheredtext');
+let decipherSubmit = document.querySelector('#deciphersubmit');
 
+
+
+//botones
 cipherSubmit.addEventListener('onclick', cipher.encode());
 decipherSubmit.addEventListener('onclick', cipher.decode());
