0e861c7b3c9bc99da9b213c289e481210b3c86a7
Maria Bolanos <Maria@Marias-MacBook-Pro.local>
2019-02-03 14:37:23 -0600

    Agregue funcionalidad de botones cifrar, descifrar, reset. El resultado ya aparece en el campo indicado y este se adecua al tamaño del texto. Agregue footer con mi información del proyecto. Tuve que hacer unos cambios en el objeto cipher para poder correr bien la pagina, por esto ya no pasa ningun test. Debo adecuar mi codigo alos lineamientos del proyecto.

--- src/cipher.js
+++ src/cipher.js
@@ -1,72 +1,73 @@
-
-let cipheredLetter = '';
-let cipheredPhrase = '';
-let decipheredLetter = '';
-let decipheredPhrase = '';
+"use strict";
 
 window.cipher = {
-    encode : (offset, string) => {
-      for (let i = 0; i < string.length; i++) {
-        let currentLetter = string.charAt(i);
-    
-        let letterCode = currentLetter.charCodeAt(0);
-    
-        if (letterCode >= 65 && letterCode <= 90) {
-          cipheredLetter = String.fromCharCode(
-            (letterCode + offset - 65) % 26 + 65
-          );
-    
-          cipheredPhrase += cipheredLetter;
-        } else if (letterCode >= 97 && letterCode <= 122) {
-          cipheredLetter = String.fromCharCode(
-            (letterCode + offset - 97) % 26 + 97
-          );
-    
-          cipheredPhrase += cipheredLetter;
-        } 
-          else {
-          cipheredLetter = String.fromCharCode(letterCode);
-    
-          cipheredPhrase += cipheredLetter;
-        }
-      }
-    
-      return cipheredPhrase;
-    },
-     
-    decode : (offset, string) => {
-      for (let i = 0; i < string.length; i++) {
-        let currentLetter = string.charAt(i);
-    
-        let letterCode = currentLetter.charCodeAt(0);
-    
-        if (letterCode >= 65 && letterCode <= 90) {
-          decipheredLetter = String.fromCharCode(
-            (letterCode - offset - 90) % 26 + 90
-          );
-    
-          decipheredPhrase += decipheredLetter;
-        } else if (letterCode >= 97 && letterCode <= 122) {
-          decipheredLetter = String.fromCharCode(
-            (letterCode - offset - 122) % 26 + 122
-          );
-    
-          decipheredPhrase += decipheredLetter;
-        } else {
-          decipheredLetter = String.fromCharCode(letterCode);
-    
-          decipheredPhrase += decipheredLetter;
-        }
-      }
-    
-      return decipheredPhrase;
-    }
-  
-  
-  
+	encode : () => {
+
+		let cipheredLetter = '';
+		let cipheredPhrase = '';
+
+		for (let i = 0; i < string.value.length; i++) {
+			let currentLetter = string.value.charAt(i);
+	
+			let letterCode = currentLetter.charCodeAt(0);
+	
+			if (letterCode >= 65 && letterCode <= 90) {
+				cipheredLetter = String.fromCharCode(
+					(letterCode + Math.abs(parseInt(offset.value)) - 65) % 26 + 65
+				);
+	
+				cipheredPhrase += cipheredLetter;
+			} else if (letterCode >= 97 && letterCode <= 122) {
+				cipheredLetter = String.fromCharCode(
+					(letterCode + Math.abs(parseInt(offset.value)) - 97) % 26 + 97
+				);
+	
+				cipheredPhrase += cipheredLetter;
+			} 
+				else {
+				cipheredLetter = String.fromCharCode(letterCode);
+	
+				cipheredPhrase += cipheredLetter;
+			}
+		}
+
+		cipheredText.innerHTML = cipheredPhrase;
+		cipheredPhrase = "";
+
+	},
+
+	decode : () => {
+
+		let decipheredLetter = '';
+		let decipheredPhrase = '';
+
+		for (let i = 0; i < string.value.length; i++) {
+			let currentLetter = string.value.charAt(i);
+	
+			let letterCode = currentLetter.charCodeAt(0);
+	
+			if (letterCode >= 65 && letterCode <= 90) {
+				decipheredLetter = String.fromCharCode(
+					(letterCode - Math.abs(parseInt(offset.value)) - 90) % 26 + 90
+				);
+	
+				decipheredPhrase += decipheredLetter;
+			} else if (letterCode >= 97 && letterCode <= 122) {
+				decipheredLetter = String.fromCharCode(
+					(letterCode - Math.abs(parseInt(offset.value)) - 122) % 26 + 122
+				);
+	
+				decipheredPhrase += decipheredLetter;
+			} else {
+				decipheredLetter = String.fromCharCode(letterCode);
+	
+				decipheredPhrase += decipheredLetter;
+			}
+		}
+	
+		cipheredText.innerHTML = decipheredPhrase;
+		decipheredPhrase = "";
+		
+	}
+	
 };
-
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
-// let string = document.querySelector('#text');
-// let offset = parseInt(document.querySelector('#choice'));
+"use strict";
+
 let cipherSubmit = document.querySelector('#ciphersubmit');
-// let cipheredText = document.querySelector('#cipheredtext');
 let decipherSubmit = document.querySelector('#deciphersubmit');
+let resetButton = document.querySelector('#reset');
 
-
+let string = document.querySelector('#text');
+let offset = document.querySelector('#choice');
+let cipheredText = document.querySelector('#cipheredtext');
 
 //botones
-cipherSubmit.addEventListener('onclick', cipher.encode());
-decipherSubmit.addEventListener('onclick', cipher.decode());
+cipherSubmit.addEventListener('click', validateFormCipher);
+decipherSubmit.addEventListener('click', validateFormDecipher);
+resetButton.addEventListener('click', resetForm);
+
+function validateFormCipher() {
+	validateFormText();
+	if (validateFormText() === true) {
+		cipher.encode();
+	}
+}
+
+function validateFormDecipher() {
+	validateFormText();
+	if (validateFormText() === true) {
+		cipher.decode();
+	}
+}
+
+function validateFormText() {
+	if (string.value.length < 1) {	
+		string.placeholder = "¡Es necesario que escribas algo!";
+		string.style.backgroundColor = "rgb(248, 126, 126)";
+		string.value = "";
+	} else {
+		string.style.backgroundColor = "rgba(121, 116, 185, 1)";
+		return true;
+	}
+}
+
+function resetForm() {
+	offset.value = "1";
+	string.value = "";
+	string.placeholder = "Escribe aquí lo que quieras cifrar o descifrar...";
+	cipheredText.innerHTML = "Aquí aparecerá tu resultado...";
+	string.style.backgroundColor = "rgba(121, 116, 185, 1)";
+	offset.style.backgroundColor = "rgba(121, 116, 185, 1)";
+}
