517fd6aabffe2d51bd55c6fb67d662790e181cdc
Maria Bolanos <Maria@Marias-MacBook-Pro.local>
2019-02-05 21:43:10 -0600

    Reduje el numero de funciones necesarias para correr el programa correctamente. Corregi el error que impedia que el resultado se imprimiera en el area correspondiente. Cambie el  nombre de div id choice por offsetnumber para identificarlo mejor y en css modifique su area en el grid para hacerlo mas pequeño y mejorar asi el layout de la pagina. Centre el texto del footer y le  quite el overflow por que me di cuenta que con el grid template row en auto, el tamaño se ajusta automaticamente al del texto.

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,18 @@
 "use strict";
+
 window.cipher = {
 	
 	encode : (offset, string) => {
-		
-		let cipheredLetter = '';
 
-		let cipheredPhrase = '';
+		let cipheredLetter;
+		let cipheredPhrase = "";
 
-		for (let i = 0; i < string.length; i++) {
-
+		for(let i = 0; i < string.length; i++) {
+			
 			let currentLetter = string.charAt(i);
 	
 			let letterCode = currentLetter.charCodeAt(0);
-	
+			
 			if (letterCode >= 65 && letterCode <= 90) {
 
 				cipheredLetter = String.fromCharCode((letterCode + Math.abs(parseInt(offset)) - 65) % 26 + 65);
@@ -31,10 +31,9 @@ window.cipher = {
 				cipheredLetter = String.fromCharCode(letterCode);
 	
 				cipheredPhrase += cipheredLetter;
-
 				}
 			
-			}	
+			}
 
 			return cipheredPhrase;
 
@@ -43,9 +42,8 @@ window.cipher = {
 
 	decode : (offset, string) => {
 
-		let decipheredLetter = '';
-
-		let decipheredPhrase = '';
+		let decipheredLetter;
+		let decipheredPhrase = "";
 
 		for (let i = 0; i < string.length; i++) {
 
--- src/index.js
+++ src/index.js
@@ -1,68 +1,72 @@
-'use strict';
+"use strict";
 
-let cipherSubmit = document.querySelector('#ciphersubmit'); // -->Llamo al elemento del botón CIFRAR en el HTML
-let decipherSubmit = document.querySelector('#deciphersubmit');// -->Llamo al elemento del botón DESCIFRAR en el HTML
-let resetButton = document.querySelector('#reset');// -->Llamo al elemento del botón RESET en el HTML
+let cipherSubmit = document.querySelector('#ciphersubmit'); // Llamo al elemento del botón CIFRAR en el HTML
+let decipherSubmit = document.querySelector('#deciphersubmit');// Llamo al elemento del botón DESCIFRAR en el HTML
+let resetButton = document.querySelector('#reset');// Llamo al elemento del botón RESET en el HTML
 
-let offsetId = document.querySelector('#choice');// -->Llamo al elemento del HTML donde el usuario elige desplazamiento
-let stringId = document.querySelector('#text');// -->Llamo al elemento del HTML donde el usuario ingresa texto
+let offsetId = document.querySelector('#offsetnumber');// Llamo al elemento del HTML donde el usuario elige desplazamiento
+let stringId = document.querySelector('#text');// Llamo al elemento del HTML donde el usuario ingresa texto
 
+let cipheredText = document.querySelector('#cipheredtext');// Llamo al elemento donde aparece el resultado esperado en el HTML
 
-let offset = document.getElementById('choice').value;// -->Llamo al valor ingresado por el usuario en el desplazamiento
-let string = document.getElementById('text').value;// -->Llamo al valor ingresado por el usuario en el campo correspondiente al texto
+cipherSubmit.addEventListener('click', validateForm);// Botón Cifrar
+decipherSubmit.addEventListener('click', validateForm);// Botón Descifrar
+resetButton.addEventListener('click', resetForm);// Botón Reset
 
-let cipheredText = document.querySelector('#cipheredtext');//-->Llamo al elemento donde aparece el resultado esperado en el HTML
+/*
 
-cipherSubmit.addEventListener('click', validateFormCipher);// --> Botón Cifrar
-decipherSubmit.addEventListener('click', validateFormDecipher);// --> Botón Descifrar
-resetButton.addEventListener('click', resetForm);// --> Botón Reset
+validateForm es la función llamada al dar click en el botón de cifrar o descifrar.
 
+Su función es validar el texto ingresado; Si el número de caracteres es menor que uno,
+cambia su placeholder a una advertencia así como el color de su fondo a rojo, y por último, 
+vuelve a dejar vacío el valor del campo donde se ingresa el texto.
 
-//Función que al darse click en botón, 
-//verifica que este ingresado algun texto y de ser correcto, imprime el resultado de la funcion encode
-function validateFormCipher() {
-	validateFormText();
-	if (validateFormText() === true) {
-		cipheredText.innerHTML = cipher.encode(offset,string);
-	}
-}
+Si pasa la prueba condicional anterior, entonces el color del fondo se cambia a su predeterminado (esto es por si se 
+equivocaron antes y el fondo del campo está ya de color rojo), y después corre la función
+cipher.decode o cipher.encode dependiendo del botón al cual se le dio el click.
 
-//Función que al darse click en botón, 
-//llama a la funcion validateFormText, y compara su resultado a que sea true.
-//De ser así, imprime el resultado de la funcion decode en el elemento cipheredText en el HTML
-function validateFormDecipher() {
-	validateFormText();
-	if (validateFormText() === true) {
-		cipheredText.innerHTML = cipher.decode(offset, string);
-	}
-}
+*/
 
-//Función que es llamada por validateForm al darse click en botón descifrar.
-//Su función es validar el texto ingresado: si es menor que uno,
-//cambia el placeholder, el color del blackground, y
-//el campo donde se ingresa el texto vuelve a estar vacío. De lo contrario,
-//el color del background se queda del mismo color y retorna true, lo cual 
-//permite que la función anterior sea completada exitosamente.
-function validateFormText() {
-	if (stringId.value.length < 1) {	
+function validateForm(event) {
+
+	let offset = offsetId.value;
+	let string = stringId.value;
+
+	if (string.length < 1) {	
 		stringId.placeholder = "¡Es necesario que escribas algo!";
 		stringId.style.backgroundColor = "rgb(248, 126, 126)";
 		stringId.value = "";
+	} else if (event.target === cipherSubmit) {
+		stringId.style.backgroundColor = "rgba(121, 116, 185, 1)";
+		cipheredText.innerHTML = cipher.encode(offset, string);
 	} else {
 		stringId.style.backgroundColor = "rgba(121, 116, 185, 1)";
-		return true;
+		cipheredText.innerHTML = cipher.decode(offset, string);
 	}
+
 }
 
-//Función que es llamada al darse click en botón RESET
-//Primero cambia el valor de offset a 1 otra vez;
-//Después regresa a ser un campo vacío el elemento donde el usuario ingresa el texto;
-//Cambia la frase del placeholder por la misma del HTML para que se vea como recién cargada;
-//Cambia la frase del elemento donde aparece el resultado por la misma del HTML para que se vea como recién cargada;
-//Cambia el color del background del elemento donde el usuario ingresa el texto, 
-//al color inicial para que se vea como recién cargada;
-//Cambia el color del background del elemento donde el usuario selecciona el desplazamiento, 
-//al color inicial para que se vea como recién cargada;
+/*
+
+Función que es llamada al darse click en botón RESET.
+
+Primero cambia el valor de offset a 1 otra vez.
+
+Después regresa a ser un campo vacío el elemento donde el usuario ingresa el texto.
+
+Cambia la frase del placeholder por la misma del HTML para que se vea como recién cargada.
+
+Cambia la frase del elemento donde aparece el resultado por la misma del HTML para que se vea 
+como recién cargada.
+
+Cambia el color del background del elemento donde el usuario ingresa el texto, al color inicial 
+para que se vea como recién cargada.
+
+Cambia el color del background del elemento donde el usuario selecciona el desplazamiento, 
+al color inicial para que se vea como recién cargada.
+
+*/
+
 function resetForm() {
 	offsetId.value = 1;
 	stringId.value = "";
